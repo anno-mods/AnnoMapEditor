6071935fd34e0f3b82e768bd1db3dacb93f4523b
Shad0wlife <matze_skywalker@hotmail.de>
2022-12-17 04:02:53 +0100

    Allow a Region to pull Islands from multiple paths
    A Pool now contains one or multiple FilePathRanges, that can be continuously indexed.
    
    This allows for DLC12 New World islands to be used and for the Old World Large Islands to include the community island.

--- AnnoMapEditor/MapTemplates/Region.cs
+++ AnnoMapEditor/MapTemplates/Region.cs
@@ -16,14 +16,35 @@ namespace AnnoMapEditor.MapTemplates
             {
                 [IslandSize.Small] = new Pool("data/sessions/islands/pool/moderate/moderate_s_{0}/moderate_s_{0}.a7m", 12),
                 [IslandSize.Medium] = new Pool("data/sessions/islands/pool/moderate/moderate_m_{0}/moderate_m_{0}.a7m", 9),
-                [IslandSize.Large] = new Pool("data/sessions/islands/pool/moderate/moderate_l_{0}/moderate_l_{0}.a7m", 14)
+                [IslandSize.Large] = new Pool(
+                    new FilePathRange[]
+                    {
+                        new FilePathRange("data/sessions/islands/pool/moderate/moderate_l_{0}/moderate_l_{0}.a7m", 1, 14),
+                        new FilePathRange("data/sessions/islands/pool/moderate/community_island/community_island.a7m", 1, 1)
+                    })
             });
         public static readonly Region NewWorld = new("NewWorld", "New World", "south_america_caribic_01",
             new()
             {
-                [IslandSize.Small] = new Pool("data/sessions/islands/pool/colony01/colony01_s_{0}/colony01_s_{0}.a7m", 4),
-                [IslandSize.Medium] = new Pool("data/sessions/islands/pool/colony01/colony01_m_{0}/colony01_m_{0}.a7m", 6),
-                [IslandSize.Large] = new Pool("data/sessions/islands/pool/colony01/colony01_l_{0}/colony01_l_{0}.a7m", 5)
+                [IslandSize.Small] = new Pool(
+                    new FilePathRange[]
+                    {
+                        new FilePathRange("data/sessions/islands/pool/colony01/colony01_s_{0}/colony01_s_{0}.a7m", 1, 4),
+                        new FilePathRange("data/dlc12/sessions/islands/pool/colony01/colony01_s_{0}/colony01_s_{0}.a7m", 5, 3)
+                    }),
+                [IslandSize.Medium] = new Pool(
+                    new FilePathRange[]
+                    {
+                        new FilePathRange("data/sessions/islands/pool/colony01/colony01_m_{0}/colony01_m_{0}.a7m", 1, 6),
+                        new FilePathRange("data/dlc12/sessions/islands/pool/colony01/colony01_m_{0}/colony01_m_{0}.a7m", 7, 3)
+                    }),
+                [IslandSize.Large] = new Pool(
+                    new FilePathRange[]
+                    {
+                        new FilePathRange("data/sessions/islands/pool/colony01/colony01_l_{0}/colony01_l_{0}.a7m", 1, 5),
+                        new FilePathRange("data/dlc12/sessions/islands/pool/colony01/colony01_l_{0}/colony01_l_{0}.a7m", 6, 3),
+
+                    })
             });
         public static readonly Region Arctic = new("Arctic", "Arctic", "DLC03_01",
             new()
@@ -52,21 +73,78 @@ namespace AnnoMapEditor.MapTemplates
         #region Pool Islands
         public struct Pool
         {
-            public string filePath;
-            public int size;
-            public int[]? ids;
+            public FilePathRange[] paths { get; init; }
+            public int size
+            {
+                get
+                {
+                    int sum = 0;
+                    foreach(var path in paths)
+                    {
+                        sum += path.size;
+                    }
+                    return sum;
+                }
+            }
+
+            public string GetPath(int i)
+            {
+                int rangeIdx = 0;
+                FilePathRange range = paths[rangeIdx];
+                int skipped = 0;
+                while(skipped + range.size <= i)
+                {
+                    skipped += range.size;
+                    range = paths[++rangeIdx];
+                }
+
+                return range.GetPath(i - skipped);
+            }
 
             public Pool(string filePath, int size)
             {
-                this.filePath = filePath;
-                this.size = size;
-                ids = null;
+                this.paths = new FilePathRange[]
+                {
+                    new FilePathRange(filePath, 1, size)
+                };
             }
 
             public Pool(string filePath, int[] ids)
             {
+                this.paths = new FilePathRange[]
+                {
+                    new FilePathRange(filePath, ids)
+                };
+            }
+
+            public Pool(FilePathRange[] paths)
+            {
+                this.paths = paths;
+            }
+        }
+
+        public struct FilePathRange
+        {
+            public string filePath;
+            public int size;
+            public int[] ids;
+
+            public string GetPath(int i)
+            {
+                return string.Format(filePath, string.Format("{0:00}", ids[i]));
+            }
+
+            public FilePathRange(string filePath, int start, int count)
+            {
                 this.filePath = filePath;
-                size = ids.Length;
+                this.size = count;
+                this.ids = Enumerable.Range(start, count).ToArray();
+            }
+
+            public FilePathRange(string filePath, int[] ids)
+            {
+                this.filePath = filePath;
+                this.size = ids.Length;
                 this.ids = ids;
             }
         }
@@ -84,11 +162,9 @@ namespace AnnoMapEditor.MapTemplates
         public string GetRandomIslandPath(IslandSize size)
         {
             int index = rnd.Next(1, PoolIslands[size].size);
-            var ids = PoolIslands[size].ids;
-            if (ids is not null)
-                index = ids[index];
 
-            return string.Format(PoolIslands[size].filePath, string.Format("{0:00}", index));
+            string path = PoolIslands[size].GetPath(index);
+            return path;
         }
 
         public override string ToString() => value;
