486c6f9ee3bffc70019bdd910473105e5277e943
Christopher Cyclonit Klinge <christ.klinge@web.de>
2022-12-30 08:24:29 +0100

    Extracted FilePathRange and Pool into separate files.

---
+++ AnnoMapEditor/MapTemplates/FilePathRange.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace AnnoMapEditor.MapTemplates
+{
+    public struct FilePathRange
+    {
+        public string filePath;
+        public int size;
+        public int[] ids;
+
+        public string GetPath(int i)
+        {
+            return string.Format(filePath, string.Format("{0:00}", ids[i]));
+        }
+
+        public FilePathRange(string filePath, int start, int count)
+        {
+            this.filePath = filePath;
+            this.size = count;
+            this.ids = Enumerable.Range(start, count).ToArray();
+        }
+
+        public FilePathRange(string filePath, int[] ids)
+        {
+            this.filePath = filePath;
+            this.size = ids.Length;
+            this.ids = ids;
+        }
+    }
+}
---
+++ AnnoMapEditor/MapTemplates/Pool.cs
@@ -0,0 +1,54 @@
+﻿namespace AnnoMapEditor.MapTemplates
+{
+    public class Pool
+    {
+        public FilePathRange[] paths { get; init; }
+        public int size
+        {
+            get
+            {
+                int sum = 0;
+                foreach(var path in paths)
+                {
+                    sum += path.size;
+                }
+                return sum;
+            }
+        }
+
+        public string GetPath(int i)
+        {
+            int rangeIdx = 0;
+            FilePathRange range = paths[rangeIdx];
+            int skipped = 0;
+            while(skipped + range.size <= i)
+            {
+                skipped += range.size;
+                range = paths[++rangeIdx];
+            }
+
+            return range.GetPath(i - skipped);
+        }
+
+        public Pool(string filePath, int size)
+        {
+            this.paths = new FilePathRange[]
+            {
+                new FilePathRange(filePath, 1, size)
+            };
+        }
+
+        public Pool(string filePath, int[] ids)
+        {
+            this.paths = new FilePathRange[]
+            {
+                new FilePathRange(filePath, ids)
+            };
+        }
+
+        public Pool(FilePathRange[] paths)
+        {
+            this.paths = paths;
+        }
+    }
+}
