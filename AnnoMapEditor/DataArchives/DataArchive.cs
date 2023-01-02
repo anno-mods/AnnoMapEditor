39efd7aa34a20fc9a2070025e08ecde1196d9088
Christopher Cyclonit Klinge <christ.klinge@web.de>
2023-01-02 15:56:06 +0100

    Renamed InvalidDataPath to InvalidDataArchive to match other archives. Moved archive related classes into their own files

--- AnnoMapEditor/DataArchives/DataArchive.cs
+++ AnnoMapEditor/DataArchives/DataArchive.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
 
 namespace AnnoMapEditor.DataArchives
 {
     public static class DataArchive
     {
-        public static readonly IDataArchive Default = new InvalidDataPath("");
+        public static readonly IDataArchive Default = new InvalidDataArchive("");
 
         public static async Task<IDataArchive> OpenAsync(string? folderPath)
         {
@@ -42,38 +40,6 @@ namespace AnnoMapEditor.DataArchives
             if (Directory.Exists(Path.Combine(path, "dlc01")))
                 return Path.GetDirectoryName(path);
             return null;
-        }
-    }
-
-    public interface IDataArchive
-    {
-        bool IsValid { get; }
-        string Path { get; }
-
-        Stream? OpenRead(string filePath);
-        IEnumerable<string> Find(string pattern);
-        Task LoadAsync();
-    }
-
-    public class InvalidDataPath : IDataArchive
-    {
-        public bool IsValid { get; } = false;
-        public Stream? OpenRead(string filePath) => null;
-        public string Path { get; }
-
-        public InvalidDataPath(string path)
-        {
-            Path = path;
-        }
-
-        public Task LoadAsync()
-        {
-            return Task.Run(() => { });
-        }
-
-        public IEnumerable<string> Find(string pattern)
-        {
-            return Array.Empty<string>();
         }
     }
 }
--- AnnoMapEditor/DataArchives/RdaDataArchive.cs
+++ AnnoMapEditor/DataArchives/RdaDataArchive.cs
@@ -10,31 +10,6 @@ using RDAExplorer;
 
 namespace AnnoMapEditor.DataArchives
 {
-    public static class RDAExtensions
-    {
-        public static RDAFile? GetFileByPath(this RDAReader that, string path)
-        {
-            Queue<string>? parts = new(Path.GetDirectoryName(path)?.Split('\\') ?? Array.Empty<string>());
-            if (!parts.Any())
-                return null;
-
-            RDAFolder? currentFolder = that.rdaFolder;
-
-            while (parts.Count > 0 && currentFolder is not null)
-            {
-                var part = parts.Dequeue();
-                currentFolder = currentFolder.Folders.FirstOrDefault(x => x.Name == part);
-            }
-
-            if (currentFolder is null)
-                return null;
-
-            var fileName = Path.GetFileName(path);
-            RDAFile? file = currentFolder.Files.FirstOrDefault(x => Path.GetFileName(x.FileName) == fileName);
-            return file;
-        }
-    }
-
     public class RdaDataArchive : IDataArchive, IDisposable
     {
         public string Path { get; }
