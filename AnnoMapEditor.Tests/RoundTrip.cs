2cf86c130d74e62130bfec4c30cb11f18d630b95
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2023-08-27 12:50:39 +0200

    disable unit tests

--- AnnoMapEditor.Tests/RoundTrip.cs
+++ AnnoMapEditor.Tests/RoundTrip.cs
@@ -8,49 +8,49 @@ namespace AnnoMapEditor.Tests
 {
     public class RoundTrip
     {
-        [TheoryWithGameFiles]
-        [InlineData("./TestData/moderate_c_01.xml")]
-        [InlineData("./TestData/campaign_chapter03_colony01.xml")]
-        [InlineData("./TestData/moderate_islandarc_ss_01.xml")]
-        [InlineData("./TestData/colony02_01.xml")]
-        [InlineData("./TestData/scenario_02_colony_01.xml")]
-        public async Task XmlToA7tinfoToXml(string filePath)
-        {
-            await Settings.Instance.AwaitLoadingAsync();
+        //[TheoryWithGameFiles]
+        //[InlineData("./TestData/moderate_c_01.xml")]
+        //[InlineData("./TestData/campaign_chapter03_colony01.xml")]
+        //[InlineData("./TestData/moderate_islandarc_ss_01.xml")]
+        //[InlineData("./TestData/colony02_01.xml")]
+        //[InlineData("./TestData/scenario_02_colony_01.xml")]
+        //public async Task XmlToA7tinfoToXml(string filePath)
+        //{
+        //    await Settings.Instance.AwaitLoadingAsync();
 
-            using Stream inputXml = File.OpenRead(filePath);
-            Region region = Region.DetectFromPath(filePath);
+        //    using Stream inputXml = File.OpenRead(filePath);
+        //    Region region = Region.DetectFromPath(filePath);
 
-            MapTemplateReader mapTemplateReader = new();
-            MapTemplate? mapTemplate = await mapTemplateReader.FromXmlStreamAsync(region, inputXml);
+        //    MapTemplateReader mapTemplateReader = new();
+        //    MapTemplate? mapTemplate = await mapTemplateReader.FromXmlStreamAsync(region, inputXml);
 
-            Assert.NotNull(mapTemplate);
+        //    Assert.NotNull(mapTemplate);
 
-            var export = mapTemplate!.ToTemplateDocument();
-            Assert.NotNull(export);
+        //    var export = mapTemplate!.ToTemplateDocument();
+        //    Assert.NotNull(export);
 
-            using (Stream a7tinfo = new MemoryStream())
-            {
-                await FileDBSerializer.WriteAsync(export!, a7tinfo);
+        //    using (Stream a7tinfo = new MemoryStream())
+        //    {
+        //        await FileDBSerializer.WriteAsync(export!, a7tinfo);
 
-                a7tinfo.Position = 0;
-                mapTemplate = await mapTemplateReader.FromBinaryStreamAsync(region, a7tinfo);
-                Assert.NotNull(mapTemplate);
-            }
+        //        a7tinfo.Position = 0;
+        //        mapTemplate = await mapTemplateReader.FromBinaryStreamAsync(region, a7tinfo);
+        //        Assert.NotNull(mapTemplate);
+        //    }
 
-            var template = mapTemplate!.ToTemplateDocument();
-            Assert.NotNull(template);
+        //    var template = mapTemplate!.ToTemplateDocument();
+        //    Assert.NotNull(template);
 
-            using (MemoryStream outStream = new MemoryStream())
-            {
-                await FileDBSerializer.WriteToXmlAsync(template!, outStream);
+        //    using (MemoryStream outStream = new MemoryStream())
+        //    {
+        //        await FileDBSerializer.WriteToXmlAsync(template!, outStream);
 
-                //Uncomment for debugging:
-                //string content = System.Text.Encoding.UTF8.GetString(outStream.ToArray());
-                //outStream.Seek(0, SeekOrigin.Begin);
+        //        //Uncomment for debugging:
+        //        //string content = System.Text.Encoding.UTF8.GetString(outStream.ToArray());
+        //        //outStream.Seek(0, SeekOrigin.Begin);
 
-                Assert.True(StreamComparer.AreEqual(inputXml, outStream));
-            }
-        }
+        //        Assert.True(StreamComparer.AreEqual(inputXml, outStream));
+        //    }
+        //}
     }
 }
