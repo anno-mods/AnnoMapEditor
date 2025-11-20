ccdd79d5c434123bb6a01527c7778a35e57f8d23
Maximilian Goldschmidt <ixam97@ixam97.de>
2025-11-21 00:06:08 +0100

    Don't show "Unsupported game" when no game path was entered yet

---
+++ AnnoMapEditor/UI/Converters/EmptyStringToVisibility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AnnoMapEditor.UI.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class EmptyStringToVisibility : IValueConverter
+    {
+        public Visibility OnEmpty { get; set; } = Visibility.Collapsed;
+        public Visibility OnNotEmpty { get; set; } = Visibility.Visible;
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not string s)
+                return null;
+            return string.IsNullOrEmpty(s) ? OnEmpty : OnNotEmpty;
+
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
